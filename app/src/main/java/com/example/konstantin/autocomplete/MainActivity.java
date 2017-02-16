6f5694aa6ffb4e9a54c3d292d245a8454c8ae697
DESKTOP-FAADA6L\Konstantin <k00190580@student.lit.ie>
2017-02-16 19:22:19 +0000

    autocomplete finished

---
+++ app/src/main/java/com/example/konstantin/autocomplete/MainActivity.java
@@ -0,0 +1,102 @@
+package com.example.konstantin.autocomplete;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Filter;
+import android.widget.Filterable;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import static android.support.v7.appcompat.R.styleable.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    AutoCompleteTextView autocompleteView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        autocompleteView = (AutoCompleteTextView) findViewById(R.id.autocomplete);
+        autocompleteView.setAdapter(new PlacesAutoCompleteAdapter(this, R.layout.autocomplete_list_item));
+
+        autocompleteView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // Get data associated with the specified position
+                // in the list (AdapterView)
+                String description = (String) parent.getItemAtPosition(position);
+                Toast.makeText(getBaseContext(), description, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+    class PlacesAutoCompleteAdapter extends ArrayAdapter<String> implements Filterable {
+
+        ArrayList<String> resultList;
+
+        Context mContext;
+        int mResource;
+
+        PlaceAPI mPlaceAPI = new PlaceAPI();
+
+        public PlacesAutoCompleteAdapter(Context context, int resource) {
+            super(context, resource);
+
+            mContext = context;
+            mResource = resource;
+        }
+
+        @Override
+        public int getCount() {
+            // Last item will be the footer
+            return resultList.size();
+        }
+
+        @Override
+        public String getItem(int position) {
+            return resultList.get(position);
+        }
+
+        @Override
+        public Filter getFilter() {
+            Filter filter = new Filter() {
+                @Override
+                protected FilterResults performFiltering(CharSequence constraint) {
+                    FilterResults filterResults = new FilterResults();
+                    if (constraint != null) {
+                        resultList = mPlaceAPI.autocomplete(constraint.toString());
+
+                        filterResults.values = resultList;
+                        filterResults.count = resultList.size();
+                    }
+
+                    return filterResults;
+                }
+
+                @Override
+                protected void publishResults(CharSequence constraint, FilterResults results) {
+                    if (results != null && results.count > 0) {
+                        notifyDataSetChanged();
+                    }
+                    else {
+                        notifyDataSetInvalidated();
+                    }
+                }
+            };
+
+            return filter;
+        }
+    }
+
+
+
+}
